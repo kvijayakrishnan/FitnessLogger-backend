daa91d885c772d2d666f8ca806b5fd03a9585b5e
kvijayakrishnan <vijayakrishnankbv@yahoo.in>
2023-03-10 15:41:12 +0530

    schema as change

--- model/fitness.model.js
+++ model/fitness.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const fitnessSchema = new mongoose.Schema({
     date:{
         type: String,
-        // require: true
+        require: true
     },
     name:{
         type: String,
@@ -15,11 +15,11 @@ const fitnessSchema = new mongoose.Schema({
     },
    workout:{
         type:String,
-        // require:true
+        require:true
     },
     burnedCalorie:{
         type:String,
-        // require:true,
+        require:true,
     }
 });
 
