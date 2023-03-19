f8bf5d00cbec40c953e99c8b33ca5a4c120efa27
kvijayakrishnan <vijayakrishnankbv@yahoo.in>
2023-03-19 19:27:47 +0530

    Add log update and delete

--- routes/fitness.routes.js
+++ routes/fitness.routes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 
 // to get fitness dashboard
-router.get('/get', async(req,res)=>{
+router.get('/fitness', async(req,res)=>{
     try{
             const data = await Fitness.find();
             res.status(200).send(data);
@@ -18,7 +18,7 @@ router.get('/get', async(req,res)=>{
 
 // Add new fitness log
 
-router.post('/post', async(req,res) => {
+router.post('/fitness', async(req,res) => {
     const data = req.body;
     const fit = new Fitness(data);
    try{
@@ -28,6 +28,39 @@ router.post('/post', async(req,res) => {
     res.status(500).send({message:'Internal server error'})
    }
 });
+
+
+
+// To update an fitness log
+router.put('/fitness/:fitID', async (req,res) => {
+    try{
+        const fitnessID = req.params.fitID;
+        await Fitness.findOneAndUpdate({_id: fitnessID},{$set:req.body})
+             res.status(200).send({id:req.params.fitID, message:"Fitness log has updated successfully.. "});
+    }catch(err){
+        console.log(err)
+        res.status(500).send({message:"Internal server error"})
+    }
+})
+
+
+
+// delete log
+router.delete('/fitness/:fitID', async (req, res) =>{
+    try{
+        const fitnessID = req.params.fitID;
+        
+       await Fitness.deleteOne({_id:fitnessID})
+                   res.status(200).send({message:"Fitness log is deleted successfully..."})
+    }catch(err){
+        console.log(err)
+        res.status(500).send({message:"Internal server error"})
+    }
+})
+
+
+
+
 
 
 
